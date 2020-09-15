e1a42e4a037b589b448ae9032b33bafe15f6f0df
Jonathan Lacson <lacson@vt.edu>
2020-09-14 22:23:13 -0400

    write a multiplicative inverse function

--- utility.py
+++ utility.py
@@ -25,4 +25,30 @@ def euler(num: int, debug=False) -> int:
         print([1] + ans)
     return len(ans) + 1
 
-    
+
+def mult_inv(bound: int) -> dict:
+    '''
+    Given a number bound, finds all the 
+    multiplicative inverses
+    from {0 .. num}.
+
+    :param bound: range to find, e.g. 26 would be {0 .. 25}
+    :return: dictionary of numbers and bounds
+    '''
+    # make dictionary to return
+    ret = {}
+
+    # we know 1 and bound-1 are mult inverses of each other
+    ret[1] = 1
+    ret[bound-1] = bound-1
+
+    # iterate through the remaining values
+    for i in range(2, bound-1):
+        if math.gcd(i, bound) == 1 and i not in ret.values():
+            # this only works on Py3.8+!
+            ans = pow(i, -1, bound)
+            ret[i] = ans
+            ret[ans] = i
+
+    # return a sorted one
+    return {key: value for key, value in sorted(ret.items(), key=lambda item: item[0])}
